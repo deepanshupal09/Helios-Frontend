e8e0551d69b33beca27a2cb6381a026df61b27f1
Deepanshu Pal <dp5114@dseu.ac.in>
2024-12-05 01:34:56 +0530

    socket fix

--- actions/websocket.ts
+++ actions/websocket.ts
@@ -14,7 +14,8 @@ export const getSocket = (): Socket | null => {
 const initializeSocket = async (): Promise<void> => {
   if (!socket) {
     try {
-      socket = io(process.env.BACKEND_URL || "http://localhost:8000");
+        console.log("pro ", process.env.NEXT_PUBLIC_BACKEND_URL)
+      socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000");
       socket.emit("join_room", process.env.ROOM_CODE || "capibara");
     } catch (error) {
       console.error("Error initializing socket:", error);
